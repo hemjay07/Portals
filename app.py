e1067b183fa587070166814eb953e02fdbe72432
hemjay07 <mujeebopabode07@gmail.com>
2023-08-18 21:52:03 +0100

    ...

--- app.py
+++ app.py
@@ -51,22 +51,52 @@ format = """
 """
 
 def roadmap(prompt, model="gpt-3.5-turbo"):
+#   try:
+#   #Make your OpenAI API request here
+#   response = openai.Completion.create(prompt="Hello world",
+#                                       model="text-davinci-003")
+# except openai.error.APIError as e:
+#   #Handle API error here, e.g. retry or log
+#   print(f"OpenAI API returned an API Error: {e}")
+#   pass
+# except openai.error.APIConnectionError as e:
+#   #Handle connection error here
+#   print(f"Failed to connect to OpenAI API: {e}")
+#   pass
+# except openai.error.RateLimitError as e:
+#   #Handle rate limit error (we recommend using exponential backoff)
+#   print(f"OpenAI API request exceeded rate limit: {e}")
+#   pass
+  try:
+      
 
-  messages = [{"role": "user", "content": prompt}]
+    messages = [{"role": "user", "content": prompt}]
 
-  response = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
 
-  model=model,
+    model=model,
 
-  messages=messages,
+    messages=messages,
 
-  temperature=0.4,
+    temperature=0.4,
 
-  )
+    )
 
-  return response.choices[0].message["content"]
+    return response.choices[0].message["content"]
+  except openai.error.APIError as e:
+    #Handle API error here, e.g. retry or log
+    print(f"OpenAI API returned an API Error: {e}")
+    pass
+  except openai.error.APIConnectionError as e:
+  #Handle connection error here
+    print(f"Failed to connect to OpenAI API: {e}")
+    pass
+  except openai.error.RateLimitError as e:
+  #Handle rate limit error (we recommend using exponential backoff)
+    print(f"OpenAI API request exceeded rate limit: {e}")
+    pass
 
-print('Running!')
+
 
 @app.route('/api/generate', methods=['POST'])
 def main():
