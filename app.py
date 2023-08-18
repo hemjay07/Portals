5a732d3f4328750bdcc6c40f08f2817dabede4c2
hemjay07 <mujeebopabode07@gmail.com>
2023-08-18 20:34:22 +0100

    ...

--- app.py
+++ app.py
@@ -66,32 +66,32 @@ print('Running!')
 
 @app.route('/api/generate', methods=['POST'])
 def main():
-  data = request.json
-  name = data['personalInfo']['fullName']
-  location = data['personalInfo']['location']
-  whatIsYourCurrentRole = data['background']['whatIsYourCurrentRole']
-  status = data['background']['status']
-  whatHaveYouLearntSoFar = data['experience']['whatHaveYouLearntSoFar']
-  whatAreYouLearningNow = data['experience']['whatAreYouLearningNow']
-  workExperience = data['experience']['workExperience']
-  careerGoal = data['careerGoal']['careerGoal']
-  tech = data['background']['whatPartOfTechAreYouInterestedIn']
+#   data = request.json
+#   name = data['personalInfo']['fullName']
+#   location = data['personalInfo']['location']
+#   whatIsYourCurrentRole = data['background']['whatIsYourCurrentRole']
+#   status = data['background']['status']
+#   whatHaveYouLearntSoFar = data['experience']['whatHaveYouLearntSoFar']
+#   whatAreYouLearningNow = data['experience']['whatAreYouLearningNow']
+#   workExperience = data['experience']['workExperience']
+#   careerGoal = data['careerGoal']['careerGoal']
+#   tech = data['background']['whatPartOfTechAreYouInterestedIn']
 
-  prompt = f"""
-  Hi, my name is {name}, and I live in{location}. I currently work as {whatIsYourCurrentRole} and I am interested in {tech}. I am currently {status}. So far, I have learnt {whatHaveYouLearntSoFar}. I am currently learning {whatAreYouLearningNow}. I have worked as {workExperience}. My career goal is {careerGoal}. 
+#   prompt = f"""
+#   Hi, my name is {name}, and I live in{location}. I currently work as {whatIsYourCurrentRole} and I am interested in {tech}. I am currently {status}. So far, I have learnt {whatHaveYouLearntSoFar}. I am currently learning {whatAreYouLearningNow}. I have worked as {workExperience}. My career goal is {careerGoal}. 
   
-  Please share a comprehensive, practical and detailed roadmap in the form of an array of objects with each object being a resource(be it a book, course, article, video, documentation, podcast) containing a resourceTitle which is the title of the resource, then a resourceLink which is the url link to the resource, then resourceDetails which summarizes and goes into some detail on the resource and what you'd learn using it, then projects as an array of objects you can build with the knowledge you have gained from the resource and then the allocatedTime which is the time taken to complete the resource. 
+#   Please share a comprehensive, practical and detailed roadmap in the form of an array of objects with each object being a resource(be it a book, course, article, video, documentation, podcast) containing a resourceTitle which is the title of the resource, then a resourceLink which is the url link to the resource, then resourceDetails which summarizes and goes into some detail on the resource and what you'd learn using it, then projects as an array of objects you can build with the knowledge you have gained from the resource and then the allocatedTime which is the time taken to complete the resource. 
 
-  Please prioritize suggesting resources that people have used and recommended in high regard. Also be more inclined towards courses, videos and documentation. 
+#   Please prioritize suggesting resources that people have used and recommended in high regard. Also be more inclined towards courses, videos and documentation. 
 
-  Let the resources you share be the best in order to help the person achieve this goal - {careerGoal}.
+#   Let the resources you share be the best in order to help the person achieve this goal - {careerGoal}.
   
-  Please give me a response in JSON using the format below.
-  Format: ```{format}```
-  """
+#   Please give me a response in JSON using the format below.
+#   Format: ```{format}```
+#   """
 
-  result = roadmap(prompt)
-  return result
+#   result = roadmap(prompt)
+  return openai.api_key
 
 
 
